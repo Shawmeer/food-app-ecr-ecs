c72521cb2b067a8cd7d74e04b84f1979507231e1
shawmeer <snasri898@gmail.com>
2025-05-30 13:15:48 +0545

    jfnakdsk

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,5 +5,5 @@ module "vpc" {
   azs     = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  enable_nat_gateway = true
+  enable_nat_gateway = false
 }
